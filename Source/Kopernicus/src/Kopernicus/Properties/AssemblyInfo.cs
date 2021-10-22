84063d8b653fe7d66f2939dc4ec5265d6e3c5a11
R-T-B <21GunSoftware@comcast.net>
2021-10-22 14:51:21 -0700

    Poke it and see if it sticks.

--- Source/Kopernicus/src/Kopernicus/Properties/AssemblyInfo.cs
+++ Source/Kopernicus/src/Kopernicus/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using Kopernicus.RuntimeUtility;
 [assembly: KSPAssemblyDependency("Kopernicus.Parser", 2, 0)]
 [assembly: KSPAssemblyDependency("ModularFlightIntegrator", 1, 0)]
 [assembly: AssemblyVersion("2.0.0")]
-
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: LogAggregator("GameData/ModuleManager.ConfigCache")]
 [assembly: LogAggregator("Logs/DART/")]
 [assembly: LogAggregator("KSP.log")]
